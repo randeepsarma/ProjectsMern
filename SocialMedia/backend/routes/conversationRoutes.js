5f7acaa901456e9f0a0d60bcdfc617137033015d
Randeep Sarma <92437704+sarmaRandeepGithub12345@users.noreply.github.com>
2023-06-05 01:26:32 +0530

    Delete SocialMedia directory

--- SocialMedia/backend/routes/conversationRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { newConversation, getConversation, getConversationsOfUser, deleteConversation } from '../controllers/conversation-controller.js'
-const router = express.Router()
-
-router.post('/add', newConversation)
-router.post('/get', getConversation)
-router.post('/getAll', getConversationsOfUser)
-router.delete('/delete', deleteConversation)
-export default router;
