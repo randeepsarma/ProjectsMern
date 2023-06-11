44da572b0738465ac0912f97bb3616ea5f012f89
Randeep Sarma <92437704+sarmaRandeepGithub12345@users.noreply.github.com>
2023-06-11 19:40:35 +0530

    Update userAuth.js

--- SocialMediaApp/front/src/api/userAuth.js
+++ SocialMediaApp/front/src/api/userAuth.js
@@ -25,7 +25,7 @@ export const loginUser = async (user) => {
             'Content-Type': 'application/x-www-form-urlencoded',
         },
     })
-    console.log(response.data)
+    //console.log(response.data)
     return response;
 }
 
