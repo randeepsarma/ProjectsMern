b9e08153354e7147f209d2c786d0a87f9d348515
Randeep Sarma <92437704+sarmaRandeepGithub12345@users.noreply.github.com>
2023-06-11 16:50:51 +0530

    Update index.js

--- SocialMediaApp/backend/index.js
+++ SocialMediaApp/backend/index.js
@@ -101,7 +101,7 @@ const corsOptions ={
 }
 //invoking cross origin resource sharing policies
 app.use(cors(corsOptions))
-app.get('/',(req,res)=>{
+app.use('/',(req,res)=>{
    res.json({"message":"This is a new message"})
 })
 app.use('/user', userRoutes)
