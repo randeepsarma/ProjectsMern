7f6249dbc1a4adb99a34921fca22c40a45aa0202
Randeep Sarma <92437704+sarmaRandeepGithub12345@users.noreply.github.com>
2023-06-11 19:35:01 +0530

    Update index.js

--- SocialMediaApp/backend/index.js
+++ SocialMediaApp/backend/index.js
@@ -95,7 +95,7 @@ app.use(bodyParser.urlencoded({ limit: "40mb", extended: true }))
 app.use(bodyParser.json({ limit: "40mb", extended: true }))
 
 const corsOptions ={
-  origin: 'https://projects-mern-qrituh1u7-sarmarandeepgithub12345.vercel.app' , 
+  origin: 'https://projects-mern.vercel.app/' , 
   credentials:true,            //access-control-allow-credentials:true
   optionSuccessStatus:200
 }
