08cecc6dbeb9e2fda6b1d3918037a68f6e3c0bda
Randeep Sarma <92437704+sarmaRandeepGithub12345@users.noreply.github.com>
2023-06-12 14:00:15 +0530

    Update functions.js

--- SocialMediaApp/backend/utils/functions.js
+++ SocialMediaApp/backend/utils/functions.js
@@ -1,6 +1,10 @@
 import { v2 as cloudinary } from 'cloudinary'
 export const destroyImage =async(url)=>{
-    
+   cloudinary.config({
+  cloud_name: process.env.cloud_name,
+  api_key: process.env.Cloud_api_key,
+  api_secret: process.env.api_secret
+});
     const urlArray = url.split('/')
     const image = urlArray[urlArray.length - 2] + "/" + urlArray[urlArray.length - 1]
 
